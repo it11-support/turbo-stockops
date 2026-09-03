d8288441a90cffa759eba1adaaa83a0c03b5be3b
IT Sysdev Rco <it11.support@rcoid.com>
2026-09-03 13:59:48 +0800

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,4 +15,7 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
   },
-})
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(mode === 'development' ? 'development' : 'production'),
+  },
+}))
